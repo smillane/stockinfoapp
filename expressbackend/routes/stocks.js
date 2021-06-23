917fc43accdb4b96e2010835e52b698fe959ae42
Sean Millane <jmillaneiv@gmail.com>
2021-06-22 23:28:01 -0400

    rds connection

--- expressbackend/routes/stocks.js
+++ expressbackend/routes/stocks.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 const result = dotenv.config({ path: '.env.local' })
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
 // error logging for dotenv
 if (result.error) {
@@ -12,7 +12,7 @@ if (result.error) {
 }
 
 // connect to aws postgres db
-const client = new Client({  
+const pool  = new Pool({  
   host: process.env.RDS_HOSTNAME,
   port: process.env.RDS_PORT,
   database: process.env.RDS_DBNAME,
@@ -21,7 +21,7 @@ const client = new Client({
 });
 
 function query() {
-  client.connect(err => {
+  pool.connect(err => {
   if (err) {
     console.error('connection error', err.stack)
   } else {
@@ -29,8 +29,10 @@ function query() {
   }
   });
 
-  client.end(err => {
-    console.log('client has disconnected')
+  console.log('this is working')
+
+  pool.end(err => {
+    console.log('pool  has disconnected')
     if (err) {
       console.log('error during disconnection', err.stack)
     }
